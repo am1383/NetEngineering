603ab60924c30b15135f7600774497a51e8ecc7a
Amir Mohammad <am205379@gmail.com>
2025-12-24 11:18:23 +0430

    Feat(migrations) : Add migration files for OAuth and server management tables

---
+++ database/migrations/2025_12_24_051029_create_servers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('servers', function (Blueprint $table) {
+            $table->id();
+            $table->string('cpu');
+            $table->string('gpu')->nullable();
+            $table->integer('ram');
+            $table->integer('storage');
+            $table->string('os');
+            $table->decimal('price_per_hour', 10, 2);
+            $table->decimal('price_per_day', 10, 2);
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('servers');
+    }
+};
---
+++ database/migrations/2025_12_24_051051_create_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('server_id')->constrained()->cascadeOnDelete();
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->enum('rent_type', ['hourly', 'daily']);
+            $table->decimal('total_price', 10, 2);
+            $table->enum('status', ['pending', 'paid', 'active', 'finished'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
