42a8835bc753f675bcc5266ef696cda2f3ec62e2
Amir Mohammad <am205379@gmail.com>
2025-12-24 11:18:46 +0430

    Feat(translations) : Add English and Persian language files for validation, passwords, and error messages

---
+++ resources/lang/fa/errors.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'not_found' => 'موردی یافت نشد.',
+    'access_error' => '.شما به این بخش دسترسی ندارید',
+    'invalid_credentials_error' => '.اطلاعات وارد شده صحیح نمیباشد',
+    'method_not_allowed' => 'متد انتخاب شده مجاز نیست.',
+    'unauthenticated' => 'برای دسترسی باید وارد شوید.',
+    'unauthorized' => 'شما اجازه دسترسی به این بخش را ندارید.',
+    'database_error' => 'خطای پایگاه داده رخ داده است.',
+    'internal_server_error' => 'خطای داخلی سرور رخ داده است.',
+    'time_slot_unavailable' => 'اسلات زمانی مورد نظر شما در دسترس نمیباشد',
+];
