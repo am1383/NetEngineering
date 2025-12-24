abec01015c85730c77b5c88c5ad872b179958642
Amir Mohammad <am205379@gmail.com>
2025-12-24 11:23:33 +0430

    feat(provider): Register services and repositories in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,42 @@
 
 namespace App\Providers;
 
+use App\Interfaces\Repositories\{
+    ServerCredentialRepositoryInterface,
+    ReservationRepositoryInterface,
+    ServerRepositoryInterface,
+    UserRepositoryInterface,
+};
+
+use App\Interfaces\Services\{
+    LoginServiceInterface,
+    ReservationServiceInterface,
+    ServerServiceInterface,
+    UserServiceInterface,
+};
+
+use App\Repositories\{
+    ServerCredentialRepository,
+    ReservationRepository,
+    ServerRepository,
+    UserRepository
+};
+
+use App\Services\{
+    LoginService,
+    ReservationService,
+    ServerService,
+    UserService
+};
+
 use Illuminate\Support\ServiceProvider;
+
+use App\Models\{
+    Reservation,
+    ServerCredential,
+    Server,
+    User
+};
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +46,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(LoginServiceInterface::class, LoginService::class);
+        $this->app->bind(ReservationServiceInterface::class, ReservationService::class);
+        $this->app->bind(ServerServiceInterface::class, ServerService::class);
+        $this->app->bind(UserServiceInterface::class, UserService::class);
+
+        $this->app->singleton(UserRepositoryInterface::class, function ($app): UserRepository {
+            return new UserRepository($app->make(User::class));
+        });
+
+        $this->app->singleton(ServerRepositoryInterface::class, function ($app): ServerRepository {
+            return new ServerRepository($app->make(Server::class));
+        });
+
+        $this->app->singleton(ReservationRepositoryInterface::class, function ($app): ReservationRepository {
+            return new ReservationRepository($app->make(Reservation::class));
+        });
+
+        $this->app->singleton(ServerCredentialRepositoryInterface::class, function ($app): ServerCredentialRepository {
+            return new ServerCredentialRepository($app->make(ServerCredential::class));
+        });
     }
 
     /**
