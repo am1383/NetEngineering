85e17532230a60529643660ca0d6a8f57c884fd3
Amir Mohammad <am205379@gmail.com>
2026-02-10 11:57:08 +0430

    Rename(RoleMiddleware): Rename RoleMiddleware to AdminRoleMiddleware

--- app/Http/Middleware/AdminRoleMiddleware.php
+++ app/Http/Middleware/AdminRoleMiddleware.php
@@ -2,25 +2,23 @@
 
 namespace App\Http\Middleware;
 
-use App\Exceptions\UnauthorizedRoleException;
+use App\Exceptions\AccessErrorException;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class RoleMiddleware
+class AdminRoleMiddleware
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, string $role): Response
+    public function handle(Request $request, Closure $next): Response
     {
-        $user = $request->user();
-
-        throw_if(
-            ! $user or $user->role !== $role,
-            UnauthorizedRoleException::class
+        throw_unless(
+            $request->user()->isAdmin(),
+            AccessErrorException::class
         );
 
         return $next($request);
