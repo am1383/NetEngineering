55895a55942520f3d7937747d6d8898911267cf9
Amir Mohammad <am205379@gmail.com>
2026-02-10 11:55:28 +0430

    Feat(errors): Implement errors for en language

---
+++ resources/lang/en/errors.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'not_found' => 'No resource was found.',
+    'access_error' => 'You do not have access to this section.',
+    'invalid_credentials_error' => 'The provided credentials are incorrect.',
+    'method_not_allowed' => 'The selected method is not allowed.',
+    'unauthenticated' => 'You must be logged in to access this resource.',
+    'unauthorized' => 'You are not authorized to access this resource.',
+    'database_error' => 'An internal database error has occurred.',
+    'internal_server_error' => 'An internal server error has occurred.',
+    'time_slot_unavailable' => 'The selected time slot is not available.',
+];
