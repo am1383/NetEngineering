36d75d07ca6e57a4385bb190dcef75cb040f67c9
Amir Mohammad <am205379@gmail.com>
2026-02-14 17:06:12 +0430

    Rename(ServerCredentialRequest): Rename ServerCredentialRequest to StoreServerCredentialRequest

--- app/Http/Requests/StoreServerCredentialRequest.php
+++ app/Http/Requests/StoreServerCredentialRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use App\Http\Requests\Request as BaseRequest;
 
-class ServerCredentialRequest extends BaseRequest
+class StoreServerCredentialRequest extends BaseRequest
 {
     /**
      * Determine if the user is authorized to make this request.
